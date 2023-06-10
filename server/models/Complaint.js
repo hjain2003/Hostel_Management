ea1f6bdd62609f3aa5923a06622384698a8d0815
Harsh <hjain_be21@thapar.edu>
2023-06-10 22:01:52 +0530

    schemas added

---
+++ server/models/Complaint.js
@@ -0,0 +1,22 @@
+import mongoose, { model } from "mongoose";
+import { Schema } from "mongoose";
+
+const complaintSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required:true
+    },
+    description:{
+        type:String,
+        required: true,
+    },
+    // user :{
+    //     type : mongoose.Types.ObjectId,
+    //     ref: "User",
+    // }
+});
+
+
+export default model("Complaint", complaintSchema);
+//users
---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+import mongoose, { model } from "mongoose";
+import { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required:true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique : true
+    },
+    rollno:{
+        type : Number,
+        required : true
+    },
+    hostel:{
+        type: String,
+        required: true,
+    },
+    roomno: {
+        type: String,
+        required : true
+    },
+
+    password:{
+        type:String,
+        required: true
+    },
+    cpassword:{
+        type:String,
+        required: true
+    },
+    // complaints : [{
+    //     type: mongoose.Types.ObjectId, 
+    //     ref: "Post"
+    // }],
+
+});
+
+export default model("User", userSchema);
