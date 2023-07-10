814f8bfa87798dcc12c485c711f7b88de07c296e
Harsh <hjain_be21@thapar.edu>
2023-07-10 09:18:14 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React, { useState } from "react";
 import Profile from './components/Profile/Profile';
 import Announcement from './components/Announcements/Announcement';
