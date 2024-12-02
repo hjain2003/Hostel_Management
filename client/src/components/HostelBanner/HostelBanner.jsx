c8c509f17853e39a2db17109e0590e7e95cae235
Harsh <hjain_be21@thapar.edu>
2024-12-03 04:21:41 +0530

    .

--- client/src/components/HostelBanner/HostelBanner.jsx
+++ client/src/components/HostelBanner/HostelBanner.jsx
@@ -4,7 +4,7 @@ import './HostelBanner.css';
 const HostelBanner = () => {
   return (
     <div className='banner'>
-      <h1>HOSTEL FACILITIES PORTAL</h1>
+      <h1>HOSTEL FACILITIES PORTAL 2024</h1>
     </div>
   )
 }
